be8835fad67cf9f1fbb7a1b0fb429b445133c602
BBBrando <31227156+BBBrando@users.noreply.github.com>
2020-02-12 11:43:01 -0800

    forgot a bracket

--- Inventory.cpp
+++ Inventory.cpp
@@ -25,3 +25,4 @@ ostream& operator<<(ostream& stream, const Inventory& item)//streams quantities 
          << " $"
          << std::fixed << std::setprecision(2) << item.m_price;
   return stream;
+}
